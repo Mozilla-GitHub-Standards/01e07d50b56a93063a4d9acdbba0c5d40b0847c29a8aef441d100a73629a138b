892958205d3a548138b37fb2e340c88fa987ec05
Scott Idler <scott.a.idler@gmail.com>
2018-08-12 16:00:20 -0700

    initial copy|rename of files from bugzilla-cli

---
+++ dc/dc.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Digicert(object):
+    def __init__(self):
+        pass
---
+++ tests/test_dc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from dc import Digicert
+
+def test_dc():
+    dc = Digicert()
+    assert dc
